a76c10455c6bdf22b6bba2faa23477fe0eb919e7
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2025-01-04 14:23:08 -0500

    ignored sigpipe error, persistent server works correctly

--- persistant_server.c
+++ persistant_server.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <time.h>
+#include <errno.h>
 #include "pipe_networking.h"
 
 void sighandle(int signo) {
@@ -11,6 +13,8 @@ void sighandle(int signo) {
 int main() {
   int to_client;
   int from_client;
+  signal(SIGINT, sighandle);
+  signal(SIGPIPE, SIG_IGN);
 
   //send random number every second
   while (1) {
@@ -19,7 +23,7 @@ int main() {
 	  //While the pipe is open
 	  srand(time(NULL));
 	  int num = random();
-	  while (write(to_client, &num, sizeof(int))) {
+	  while (write(to_client, &num, sizeof(int)) > -1) {
 		  printf("SERVER (%d) : Sending %d\n", getpid(), num);
 		  num = random();
 		  sleep(1);
